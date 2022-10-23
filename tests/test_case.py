943cd9734554a7ecbe07621371930e6e2e2b65cd
Ajay Kumar <ajay_kumar_ggn@mckinsey.com>
2022-10-23 12:56:56 +0530

    quiz 3 for mlops

--- tests/test_case.py
+++ tests/test_case.py
@@ -12,20 +12,18 @@ import os
 
 def test_gorund_truth_labels():
     digits = datasets.load_digits()
-
     data_viz(digits)
     data, label = preprocess_digits(digits)
     x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(data, label, 0.8, 0.1)
     clf = load(".\models\svm_gamma=0.001_C=0.5.joblib") #loading best model
     predicted = clf.predict(x_test)
-    set_1 = set(y_train)
+    set_1 = set(clf.classes_)
     set_2 = set(predicted)
     assert set_1==set_2  # both set of train and predicted classes must be same
 
 
 def test_classifier_bias():
     digits = datasets.load_digits()
-
     data_viz(digits)
     data, label = preprocess_digits(digits)
     x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(data, label, 0.8, 0.1)
